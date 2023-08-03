76a3eeeec045f6685546377cc70007cce32c4634
iurych <iury.vibe@gmail.com>
2023-08-03 16:21:18 -0300

    Changed the title of the page to "Comece aqui" and removed the React and Vite logos.

---
+++ src/styles/global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+	--FeedbackAlert1: #cd2b31;
+	--FeedbackAlert2: #fdd8d8;
+	--FeedbackAlert3: #ffe5e5;
+	--FeedbackSucess1: #18794e;
+	--FeedbackSucess2: #ccebd7;
+	--FeedbackSucess3: #ddf3e4;
+	--ColorRandom1: #e34d8c;
+	--ColorRandom2: #c04277;
+	--ColorRandom3: #7d2a4d;
+	--ColorRandom4: #7000ff;
+	--ColorRandom5: #6200e3;
+	--ColorRandom6: #36007d;
+	--ColorRandom7: #349974;
+	--ColorRandom8: #2a7d5f;
+	--ColorRandom9: #153d2e;
+	--ColorRandom10: #6100ff;
+	--ColorRandom11: #5700e3;
+	--ColorRandom12: #30007d;
+	--ColorBrand1: #4529e6;
+	--ColorBrand2: #5126ea;
+	--ColorBrand3: #b0a6f0;
+	--ColorBrand4: #edeafd;
+	--GreyScale0: #0b0d0d;
+	--GreyScale1: #212529;
+	--GreyScale2: #495057;
+	--GreyScale3: #868e96;
+	--GreyScale4: #adb5bd;
+	--GreyScale5: #ced4da;
+	--GreyScale6: #dee2e6;
+	--GreyScale7: #e9ecef;
+	--GreyScale8: #f1f3f5;
+	--GreyScale9: #f8f9fa;
+	--GreyScale10: #fdfdfd;
+	--ColorsFixedWhiteFixed: #ffffff;
+}
+
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        border: none;
+        list-style: none;
+        text-decoration: none;
+
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    
+
+`;
