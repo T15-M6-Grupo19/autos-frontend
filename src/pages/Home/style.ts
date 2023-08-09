3c3c05a0e541b55dbad4cdecbfa50302f0b2ade6
Bruno120Ab <abreubruno499@gmail.com>
2023-08-08 23:48:29 -0300

    feat: add page Home

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+
+const SimulateComponentHeaderStyle = styled.header`
+     height: 15vh;
+     width: 100vw;
+
+     border: 1px solid black;
+`
+
+const ContainerButtonFilterStyle = styled.div`
+     height: 15vh;
+     width: 100vw;
+
+     margin: 15px 0;
+
+     justify-content: center;
+     flex-direction: columns;
+     align-items: center;
+     display: flex;
+`
+
+export { SimulateComponentHeaderStyle, ContainerButtonFilterStyle }
