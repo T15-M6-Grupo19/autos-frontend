73e21e32ebbafc08c8ba85ab3820c6dc31af4f87
Bruno120Ab <abreubruno499@gmail.com>
2023-08-08 23:48:04 -0300

    feat: add app and router

---
+++ src/Routers/routes.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/home" element={<Home />} /> */}
+    </Routes>
+
+  );
+}
