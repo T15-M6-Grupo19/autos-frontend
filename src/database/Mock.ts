4e8f925a43214c709475059f4282a905d7339586
lucaslamas-13 <lucaslamas13@gmail.com>
2023-08-09 14:59:12 -0300

    fix: more cars mock

--- src/database/Mock.ts
+++ src/database/Mock.ts
@@ -6,7 +6,16 @@ import Ferrari from "../assets/Ferrari.svg"
 const infoListCards: car[] = [ 
      { imageURL:`${Porshe}`, name: 'Porshe', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '}, 
      { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
-     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '}
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
+     { imageURL:`${Ferrari}`,name: 'Ferrari', description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate sequi totam illum, earum est laudantium. Dolor distinctio voluptatibus dolorum. '},
 
 ]
 
