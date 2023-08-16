c34165364ab72231aa54ec87dbf01ecf93a5f3da
Bruno120Ab <abreubruno499@gmail.com>
2023-08-16 14:16:11 -0300

    feat: add page

---
+++ src/pages/Sellers/index.tsx
@@ -0,0 +1,23 @@
+import { BannerColor } from "../../components/Banner";
+import { Footer } from "../../components/Footer";
+import ListCards from "../../components/ListCards";
+import { NavBarAdvertiser } from "../../components/NavBar";
+import Pagination from "../../components/Pagination";
+import { mockList } from "../../database/Mock2";
+import { MainStructure } from "./style";
+
+const Profile = () => {
+     return (
+          <>
+               <NavBarAdvertiser />
+               <BannerColor />
+               <MainStructure>
+                    <ListCards listCard={mockList} />
+               </MainStructure>
+               <Pagination />
+               <Footer />
+          </>
+     );
+   };
+   
+export default Profile;
