c66e6cf5771a2686a9cae177a7d25c9e5b859b2c
Bruno120Ab <abreubruno499@gmail.com>
2023-08-16 14:15:15 -0300

    feat: add other banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,17 +1,38 @@
-import {BackgroundGradientStyle, BannerStyle, TextStyle} from "./style"
+import Button from "../Button"
+import {  BannerColorStyle, BannerImageStyle,
+          BackgroundGradientStyle, SellerDataSquareStyle, 
+          TextStyle, FirstLettersSquareStyle, AdvertiserStyle
+} from "./style"
+
+const BannerImage = () => (
+     <BannerImageStyle>
+          <BackgroundGradientStyle>
+          <TextStyle>
+               <h1 className="textHeading5500">Motor Shop</h1>
+               <h2 className="textBody2500">A melhor plataforma de anúncios de carros do país</h2>
+          </TextStyle>
+          </BackgroundGradientStyle>
+     </BannerImageStyle>
+)
+
+const BannerColor = () =>  (
+     <BannerColorStyle>
+          <SellerDataSquareStyle>
+               <FirstLettersSquareStyle>
+                    SL
+               </FirstLettersSquareStyle>
+               <AdvertiserStyle>
+                    <h2 className="advertiser_name ">Samuel Leão</h2>
+                    <div className="advertiser_tag ">Anunciante</div>
+               </AdvertiserStyle>
+               <p className="descripition">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, excepturi neque suscipit sapiente odio omnis labore rerum officia sequi assumenda vero aliquid, nemo fugiat placeat.</p>
+               <Button name={"Criar anuncio"} variant="secondary"/>
+
+          </SellerDataSquareStyle>
+     </BannerColorStyle>
+
+)
 
 
-const Banner = () => {
-     return (
-          <BannerStyle>
-               <BackgroundGradientStyle>
-               <TextStyle>
-                    <h1 className="textHeading5500">Motor Shop</h1>
-                    <h2 className="textBody2500">A melhor plataforma de anúncios de carros do país</h2>
-               </TextStyle>
-               </BackgroundGradientStyle>
-          </BannerStyle>
-     )
-}
+export { BannerColor, BannerImage }
 
-export default Banner;
--- src/components/Banner/style.ts
+++ src/components/Banner/style.ts
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 import Banner from '../../assets/Banner.png';
 
-const BannerStyle = styled.section`
+const BannerImageStyle = styled.section`
   height: 600px;
   width: 100%;
 
@@ -20,6 +20,30 @@ const BannerStyle = styled.section`
   }
 `;
 
+const BannerColorStyle = styled.section`
+  height: 285px;
+  width: 100%;
+
+  background-color: var(--color-Brand1);
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 80px 0 500px 0;
+
+  position: relative;
+  z-index: 0;
+
+  transition: 1s linear;
+
+  @media screen and (min-width: 768px) {
+    margin: 80px 0 350px 0;
+  }
+`;
+
 const BackgroundGradientStyle = styled.div`
   height: 100%;
   width: 100%;
@@ -29,6 +53,101 @@ const BackgroundGradientStyle = styled.div`
   z-index: 1;
 
   background: linear-gradient(180deg, rgba(0, 0, 0, 0.29) 0%, #000000 100%);
+`;
+
+const SellerDataSquareStyle = styled.div`
+  height:  550px;
+  width: max(80%, 300px);
+
+  padding: 40px 29px 40px 29px;
+
+  background-color: var(--color-WhiteFixed);
+
+  border-radius: 4px;
+  border: 1px solid #868E9680;
+
+  position: absolute;
+  top: 75px;
+
+  flex-direction: column;
+  align-items: flex-start;
+  display: flex;
+  gap: 15px;
+
+  z-index: 1;
+
+  transition: 1s linear;
+
+  .descripition{
+    color: var(--color-Grey2);
+
+    line-height: 28px;
+    font: 400 16px "Inter";
+  }
+
+  @media screen and (min-width: 768px) {
+    height:  400px;
+  }
+`;
+
+const FirstLettersSquareStyle = styled.div`
+  height: 104px;
+  width: 104px;
+
+  background-color: var(--color-Brand1);
+  color: var(--color-WhiteFixed);
+
+  border-radius: 100%;
+  border: 1px solid var(--color-WhiteFixed);
+
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+
+  line-height: 53px;
+  font: 500 36px "Inter";
+`;
+
+const AdvertiserStyle = styled.div`
+  height: auto;
+  width: min(250px, 100%);
+
+  padding-right: 5px;
+  border: 1px solid transparent;
+
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: flex-start;
+  display: flex;
+
+  .advertiser_name {
+    line-height: 25px;
+    font: 600 20px "Lexend";
+
+    color: var(--color-Grey1);
+  }
+
+  .advertiser_tag{
+    height:  100%;
+    width: auto;
+    
+    padding: 0 10px;
+
+    background-color: #EDEAFD; 
+
+    border-radius: 4px;
+    border: 1px solid transparent;
+
+    justify-content: center ;
+    flex-direction: row;
+    align-items: center;
+    display: flex;
+
+    color: #4529E6;
+
+    font: 500 14px "Inter";
+  }
 `;
 
 const TextStyle = styled.div`
@@ -58,4 +177,9 @@ const TextStyle = styled.div`
   }
 `;
 
-export { BannerStyle, BackgroundGradientStyle, TextStyle };
+export { 
+  BannerImageStyle, BannerColorStyle,
+  BackgroundGradientStyle, SellerDataSquareStyle,
+  TextStyle, FirstLettersSquareStyle,
+  AdvertiserStyle
+};
