244d6dfa7cd9a586c1341112c77fbb4458c7ad02
Bruno120Ab <abreubruno499@gmail.com>
2023-08-16 14:16:57 -0300

    feat: add new route

--- src/Routers/routes.tsx
+++ src/Routers/routes.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Profile from "../pages/Sellers";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/home" element={<Home />} /> */}
+      <Route path="/Profile" element={<Profile />} /> 
     </Routes>
 
   );
