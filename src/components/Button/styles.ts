3c1beaba0d5595289930dac05b8a2d5387522fa5
lucaslamas-13 <lucaslamas13@gmail.com>
2023-08-17 15:31:44 -0300

    fix: Component Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,8 +1,8 @@
-import { iButtonStyles } from "./types";
+import { iButton } from "./types";
 
 import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button<iButtonStyles>`
+export const StyledButton = styled.button<iButton>`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -22,23 +22,68 @@ export const StyledButton = styled.button<iButtonStyles>`
           background-color: var(--color-Brand2);
           color: var(--color-WhiteFixed);
 
-          font-size: 1.6rem;
+          border-radius: 4px;
+
+          font-size: 1.1rem;
           font-family: "Inter";
           font-weight: 700;
           font-style: normal;
           text-decoration: none;
           text-transform: none;
-          
-          &:hover { 
-            background-color: var(--color-Grey1)
+
+          &:hover {
+            background-color: var(--color-Grey1);
           }
         `;
       case "secondary":
         return css`
-          xxx
-          :hover {
-           xxx
+          background-color: var(--color-WhiteFixed);
+          color: var(--color-Grey0);
+
+          border: 2px solid var(--color-Grey4);
+          border-radius: 4px;
+
+          font-size: 1rem;
+          font-family: "Inter";
+          font-weight: 700;
+          font-style: normal;
+          text-decoration: none;
+          text-transform: none;
+
+          &:hover {
+            background-color: var(--color-Brand2);
+            color: var(--color-WhiteFixed);
+            border: none;
           }
+        `;
+      case "registerButton":
+        return css`
+          background-color: var(--color-Brand2);
+          color: var(--color-WhiteFixed);
+
+          border-radius: 4px;
+
+          font-size: 1.1rem;
+          font-family: "Inter";
+          font-weight: 700;
+          font-style: normal;
+          text-decoration: none;
+          text-transform: none;
+
+          &:hover {
+            background-color: var(--color-Grey1);
+          }
+
+          @media (min-width: 620px) {
+            width: 320px;
+          }
+        `;
+      case "test":
+        return css`
+              XXX
+              &:hover {
+                XXX
+              }
         `;
     }
   }}
