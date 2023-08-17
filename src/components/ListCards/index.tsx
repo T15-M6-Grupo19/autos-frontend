6e24ecffa618022889be4df61900b40b36f5ca78
Bruno120Ab <abreubruno499@gmail.com>
2023-08-17 14:10:43 -0300

    feat: add list

--- src/components/ListCards/index.tsx
+++ src/components/ListCards/index.tsx
@@ -1,16 +1,23 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Card from "./Card";
 import { ListCardContainer } from "./style";
 import { CarContext } from "../../providers/CarContext";
+import { useLocation } from "react-router-dom";
 
 
 const ListCards = () => {
   const { searchResult } = useContext(CarContext)
+  const [isProfile, setIsProfile] = useState(false);
 
   
+  const page = useLocation();
+  useEffect(() => {
+    page.pathname.split('/')[1] === 'Profile' && setIsProfile(true);
+  }, [page]);
+ 
 
   return (
-    <ListCardContainer>
+    <ListCardContainer view={ isProfile ? "allSpace" : "spaceTwo"}>
       {searchResult!.map((car) => (
         <Card key={car.km} car={car} />
       ))}
