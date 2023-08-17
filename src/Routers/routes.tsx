9fce0ddc006f4c11cd2f1fd386a2483bfb7a3762
Bruno120Ab <abreubruno499@gmail.com>
2023-08-17 14:11:32 -0300

    feat: add profile

--- src/Routers/routes.tsx
+++ src/Routers/routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Profile from "../pages/Profile";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Profile" element={<Profile />} /> 
+
       {/* <Route path="/home" element={<Home />} /> */}
     </Routes>
 
