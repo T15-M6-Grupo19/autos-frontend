7c1c3423835647b3989a9a2edf6ae5758e2bb5c2
lucaslamas-13 <lucaslamas13@gmail.com>
2023-08-17 15:35:34 -0300

    feat: Register Route

--- src/Routers/routes.tsx
+++ src/Routers/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Register from "../pages/Register";
 
 export default function Router() {
   return (
     <Routes>
+      <Route path="/register" element={<Register/>}/>
       <Route path="/" element={<Home />} />
       {/* <Route path="/home" element={<Home />} /> */}
     </Routes>
