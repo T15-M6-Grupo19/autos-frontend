041df40385cf2c4badd63fc3db039995f1b9e919
iurych <iury.vibe@gmail.com>
2023-08-22 18:09:06 -0300

    feat: adiciona rotas e pag de reset

--- src/Routers/routes.tsx
+++ src/Routers/routes.tsx
@@ -7,7 +7,7 @@ import Register from '../pages/Register';
 import ProfileViewUser from '../pages/Profile/ProfileViewUser';
 import { DefaulLayout } from '../pages/RecoverPWD';
 import { MainRecover } from '../pages/RecoverPWD/MainSendInfo';
-import { ResetPassWord } from '../pages/RecoverPWD/MainReserPWD/ResetPWDForm';
+import { MainRecoverPWD } from '../pages/RecoverPWD/MainReserPWD';
 
 export default function Router() {
   return (
@@ -19,7 +19,7 @@ export default function Router() {
       <Route path='/profile/:userId' element={<ProfileViewUser />} />
       <Route path='/recover' element={<DefaulLayout />}>
         <Route path='/recover' element={<MainRecover />} />
-        <Route path='/recover/resetpwd/:token' element={<ResetPassWord />} />
+        <Route path='/recover/resetpwd/:token' element={<MainRecoverPWD />} />
       </Route>
     </Routes>
   );
